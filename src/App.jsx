e6031f89cc2aebc04e0f8f953fbd3d418e4c4ce7
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-06-02 20:35:34 +0300

    add login button to App

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import { useQuery } from "@apollo/client";
 import Authors from "./components/Authors";
 import Books from "./components/Books";
 import NewBook from "./components/NewBook";
+import LoginForm from "./components/LoginForm";
 import { ALL_AUTHORS, ALL_BOOKS } from "./queries";
 
 const App = () => {
   const [page, setPage] = useState("authors");
+  const [loggedIn, setLoggedIn] = useState(false);
 
   const result = useQuery(ALL_AUTHORS, {
     pollInterval: 2000,
@@ -25,12 +27,17 @@ const App = () => {
         <button onClick={() => setPage("authors")}>authors</button>
         <button onClick={() => setPage("books")}>books</button>
         <button onClick={() => setPage("add")}>add book</button>
+        {!loggedIn && <button onClick={() => setPage("login")}>login</button>}
       </div>
       {page === "authors" && <Authors authors={result.data.allAuthors} />}
 
       {page === "books" && <Books books={booksQuery.data.allBooks} />}
 
       {page === "add" && <NewBook />}
+
+      {page === "login" && (
+        <LoginForm setLoggedIn={setLoggedIn} setPage={setPage} />
+      )}
     </div>
   );
 };
